67353be03a92b2df876d999d01420f763b1ed750
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2023-06-21 23:22:51 -0300

    Take heap snapshot

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 const express = require('express');
+const v8Profiler = require('v8-profiler-next');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
-const PORT = 3000;
+const snapshots = new Map();
 
-app.listen(PORT, () => {
-  console.log(`Access server on  http://localhost:${PORT}`);
+// API endpoint for taking a heap snapshot
+app.get('/api/heap-snapshot', (req, res) => {
+  const snapshot = v8Profiler.takeSnapshot();
+  const name = snapshot.getHeader().title;
+  snapshots.set(name, snapshot);
+
+  res.send({ name });
+});
+
+// API endpoint for downloading a heap snapshot by name
+app.get('/api/heap-snapshot/:name', (req, res) => {
+  const name = req.params.name;
+  const snapshot = snapshots.get(name);
+  if (!snapshot) {
+    return res.status(404).send({ error: 'Snapshot not found' });
+  }
+
+  const fileName = `${name}.heapsnapshot`;
+  const filePath = path.resolve(__dirname, 'snapshots', fileName);
+  snapshot.serialize(
+    { write: chunk => fs.appendFileSync(filePath, chunk) },
+    () => {
+      res.download(filePath, fileName, error => {
+        if (error) {
+          console.error(error);
+        }
+        snapshots.delete(name);
+      });
+    }
+  );
+});
+
+// Start the Express server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Access server on  http://localhost:${port}`);
 });
